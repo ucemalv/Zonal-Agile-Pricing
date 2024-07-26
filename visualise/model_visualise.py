0a00ff530bdba5cde9c571a8cf5da7c83f723878
Clau Calvo <ucemalv@ucl.ac.uk>
2024-07-26 23:25:38 +0100

    Update model_visualise.py
    add small parameter to plot function to stop fix data error

--- visualise/model_visualise.py
+++ visualise/model_visualise.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from pandas import DataFrame
 import numpy as np
 
-def plot_all_train_test(train: DataFrame, test: DataFrame, data: DataFrame, date: DataFrame, normalizer, window_size: int) -> None:
+def plot_all_train_test(train: DataFrame, test: DataFrame, data: DataFrame, date: DataFrame, normalizer, window_size: int, small: bool) -> None:
     """plot original data vs the training predictions and the test predictions"""
     train_predictions = np.empty_like(data)
     train_predictions[:,:] = np.nan 
@@ -15,6 +15,12 @@ def plot_all_train_test(train: DataFrame, test: DataFrame, data: DataFrame, date
     test_predictions[len(train) + (window_size*2) + 1 : len(data) - 1, :] = test
 
     orig_data = normalizer.inverse_transform(data)
+
+    if small:
+        train_predictions = train_predictions[62900:63300]
+        test_predictions = test_predictions[62900:63300]
+        orig_data = orig_data[62900:63300]
+        date = date[62900:63300]
 
     # plot 
     plt.figure(figsize = (15, 15))
@@ -56,3 +62,6 @@ def plot_future_24(predictions: DataFrame, zone: str, date: DataFrame, real_data
     plt.xlabel("30 Minute Intervals")
     plt.ylabel("Agile Price (p/KWh)")
     plt.title(f"24 Hour Agile Price Predictions For {zone} Zone Against Real Data")
+
+    plt.legend()
+    plt.show()
